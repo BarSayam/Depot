8938a39fa69f01bdddadf41781414da1334cb360
SayamBar <sayam.bar@bitcanny.com>
2023-01-02 18:17:02 +0530

    Partially Done

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  # get 'admin/index'
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+  resources :users
+  resources :orders
+  resources :line_items
+  resources :carts
+  root 'store#index' ,as: 'store_index'
+  resources :products
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
